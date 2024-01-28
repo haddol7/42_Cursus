ae87e24ba93ab410ccbd9cc626856907e5c99c92
haddol7 <haddol010313@khu.ac.kr>
2024-01-28 17:47:05 +0900

    [ADD] structure 2

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,11 +6,42 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 18:57:58 by daeha             #+#    #+#             */
-/*   Updated: 2024/01/27 14:04:50 by daeha            ###   ########.fr       */
+/*   Updated: 2024/01/28 17:45:56 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	write_literally(const char *format, int len, int *result)
+{
+	int	temp;
+
+	temp = write(1, format, len);
+	if (temp < 0)
+		return (0);
+	*result += temp;
+	return (1);
+}
+
+int	write_conversion(const char *format, int *i, int *result, va_list *ap)
+{
+	char	*str;
+	char	*spe;
+	int		size;
+	t_flag	flag;
+
+	if (format[*i] == '\0')
+		return (1);
+	parse_flag(format, &flag, i);
+	if (!make_speci(&spe, ap, format, i) || \
+		!make_field(&str, spe, flag, size) || \
+		!write_literally(str, size, *result))
+		return (0);
+	if (str != NULL)
+		free(str);
+	str = NULL;
+	return (1);
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -23,15 +54,15 @@ int	ft_printf(const char *format, ...)
 	offset = 0;
 	i = 0;
 	va_start(ap, format);
-	while (1)
+	while (format[offset] == '\0')
 	{
 		if (format[offset + i] == '\0' || format[offset + i] == '%')
 		{
-			if (!write_literally(format, i, &offset, &result) ||\
-				!write_conversion(format, i, &offset, &result))
+			if (!write_literally(format + offset, i, &result) || \
+				!write_conversion(format + offset, &i, &result, &ap))
 				return (-1);
-			if (format[offset + i] == '\0')
-				break ;
+			offset = i;
+			i = 0;
 		}
 		i++;
 	}
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,19 +6,27 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 18:58:07 by daeha             #+#    #+#             */
-/*   Updated: 2024/01/27 14:06:34 by daeha            ###   ########.fr       */
+/*   Updated: 2024/01/27 16:39:07 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+
+typedef struct s_flag
+{
+	int	width;
+	int	preci;
+	int	alt;
+	int	left;
+	int	sign;
+	int	sign_p;
+	int	zero;
+}	t_flag;
 
 int	ft_printf(const char *format, ...);
-int	write_literally(const char *format, int i, int *offset, int *result);
-int	write_conversion(const char *format, int i, int *offset, int *result);
-
 #endif
