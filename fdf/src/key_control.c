c98cb5081ae25ae72f2788c6ede11b77248f456f
haddol7 <haddol010313@khu.ac.kr>
2024-03-22 20:45:07 +0900

    [ADD] better key event control

--- fdf/src/key_control.c
+++ fdf/src/key_control.c
@@ -6,11 +6,25 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 15:49:53 by daeha             #+#    #+#             */
-/*   Updated: 2024/03/22 18:56:57 by daeha            ###   ########.fr       */
+/*   Updated: 2024/03/22 20:43:50 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+static void	center_map(t_client *data)
+{
+	data->map.angular.x = 0;
+	data->map.angular.y = 0;
+	data->map.angular.z = 0;
+	data->map.translate.x = 0;
+	data->map.translate.y = 0;
+	data->map.translate.z = 0;
+	if (data->map.col > data->map.row)
+		data->map.scale = WINDOW_X_SIZE / (double)data->map.col;
+	else
+		data->map.scale = WINDOW_Y_SIZE / (double)data->map.row;
+}
 
 int	key_hook(int keycode, t_client *data)
 {
@@ -27,14 +41,7 @@ int	key_hook(int keycode, t_client *data)
 	else if (keycode == KEY_E)
 		data->map.angular.y -= 5;
 	else if (keycode == KEY_C)
-	{
-		data->map.angular.x = 0;
-		data->map.angular.y = 0;
-		data->map.angular.z = 0;
-		data->map.translate.x = 0;
-		data->map.translate.y = 0;
-		data->map.translate.z = 0;
-	}
+		center_map(data);
 	else if (keycode == KEY_ESC)
 		terminate((void *)data);
 	draw(data->map, &data->img, data->mlx, data->win);
@@ -43,6 +50,8 @@ int	key_hook(int keycode, t_client *data)
 
 int	mouse_press_hook(int keycode, int x, int y, t_client *data)
 {
+	if (data->mouse.is_pressed != 0)
+		return (0);
 	if (keycode == MOUSE_LEFT)
 	{
 		data->map.mouse_info = 1;
@@ -58,32 +67,9 @@ int	mouse_press_hook(int keycode, int x, int y, t_client *data)
 		data->mouse.y = y;
 	}
 	else if (keycode == MOUSE_UP)
-		data->map.scale += 0.5;
-	else if (keycode == MOUSE_DOWN && data->map.scale > 0)
-		data->map.scale -= 0.5;
-	draw(data->map, &data->img, data->mlx, data->win);
-	return (0);
-}
-
-int	mouse_release_hook(int keycode, int x, int y, t_client *data)
-{
-	if (keycode == MOUSE_LEFT || keycode == MOUSE_RIGHT)
-	{
-		data->mouse.is_pressed = 0;
-		data->map.mouse_info = 0;
-		if (keycode == MOUSE_LEFT)
-		{
-			data->map.angular.x += data->map.mouse_x;
-			data->map.angular.y += data->map.mouse_y;
-		}
-		else
-		{
-			data->map.translate.x += data->map.mouse_x;
-			data->map.translate.y += data->map.mouse_y;
-		}
-		data->map.mouse_x = 0;
-		data->map.mouse_y = 0;
-	}
+		data->map.scale *= 1.2;
+	else if (keycode == MOUSE_DOWN)
+		data->map.scale *= 0.5;
 	draw(data->map, &data->img, data->mlx, data->win);
 	return (0);
 }
@@ -102,6 +88,33 @@ int	mouse_drag_hook(int x, int y, t_client *data)
 		data->map.mouse_x = x - data->mouse.x;
 		data->map.mouse_y = y - data->mouse.y;
 	}
+	draw(data->map, &data->img, data->mlx, data->win);
+	return (0);
+}
+
+int	mouse_release_hook(int keycode, int x, int y, t_client *data)
+{
+	if (keycode != data->mouse.is_pressed)
+		return (0);
+	if (keycode == MOUSE_LEFT || keycode == MOUSE_RIGHT)
+	{
+		data->mouse.is_pressed = 0;
+		data->map.mouse_info = 0;
+		if (keycode == MOUSE_LEFT)
+		{
+			data->map.angular.x += data->map.mouse_x;
+			data->map.angular.y += data->map.mouse_y;
+		}
+		else
+		{
+			data->map.translate.x += data->map.mouse_x;
+			data->map.translate.y += data->map.mouse_y;
+		}
+		data->map.mouse_x = 0;
+		data->map.mouse_y = 0;
+	}
+	if (x * y == 42)
+		ft_printf("\r🥚\r");
 	draw(data->map, &data->img, data->mlx, data->win);
 	return (0);
 }
