353bcc01f6a3e38def89d2db9a7c4f75a42e2bb8
haddol7 <haddol010313@khu.ac.kr>
2024-07-11 20:52:05 +0900

    Fix: death flag print order

--- 3_Philosophers/philo/src/utils.c
+++ 3_Philosophers/philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 19:18:16 by daeha             #+#    #+#             */
-/*   Updated: 2024/07/10 20:01:20 by daeha            ###   ########.fr       */
+/*   Updated: 2024/07/11 20:36:39 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,9 +59,12 @@ int	ft_usleep(size_t ms, t_philo *philo)
 
 void	ft_print_mutex(char *msg, t_philo *philo)
 {	
-	if (is_philo_terminated(philo))
-		return ;
 	pthread_mutex_lock(philo->write);
+	if (is_philo_terminated(philo))
+	{
+		pthread_mutex_unlock(philo->write);
+		return ;
+	}
 	printf("%zu %d %s\n", ft_gettime() - philo->birth_time, philo->name, msg);
 	pthread_mutex_unlock(philo->write);
 }
