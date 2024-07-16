3da20779d7a10c9f6e245530d3c8f316980509af
haddol7 <haddol010313@khu.ac.kr>
2024-07-16 17:57:56 +0900

    Refactor: trivial modification for check starvation

--- 3_Philosophers/philo_bonus/procs_monitor_bonus.c
+++ 3_Philosophers/philo_bonus/procs_monitor_bonus.c
@@ -6,22 +6,24 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 21:40:30 by daeha             #+#    #+#             */
-/*   Updated: 2024/07/16 17:49:05 by daeha            ###   ########.fr       */
+/*   Updated: 2024/07/16 17:57:00 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
-static int	is_philo_starving(t_stat *stat);
+static int	is_philo_starving(t_stat *stat, size_t time_do_die);
 
 void	*monitoring(void *arg)
 {
 	t_stat	*stat;
+	size_t	time_to_die;
 
 	stat = (t_stat *)arg;
+	time_to_die = stat->time_to_die;
 	while (TRUE)
 	{
-		if (is_philo_starving(stat))
+		if (is_philo_starving(stat, time_to_die))
 		{
 			sem_wait(stat->write);
 			printf("%zu %d died\n", \
@@ -38,13 +40,13 @@ void	*monitoring(void *arg)
 	return (arg);
 }
 
-static int	is_philo_starving(t_stat *stat)
+static int	is_philo_starving(t_stat *stat, size_t time_to_die)
 {
 	t_philo	philo;
 
 	philo = stat->philo;
 	sem_wait(philo.unique_eat);
-	if (ft_gettime() - philo.last_meal > philo.time_to_die)
+	if (ft_gettime() - philo.last_meal > time_to_die)
 	{
 		sem_post(philo.unique_eat);
 		return (TRUE);
