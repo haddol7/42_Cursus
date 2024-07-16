673d281501ecf09966d015eb1f99547c06fbe4c9
haddol7 <haddol010313@khu.ac.kr>
2024-07-16 16:42:48 +0900

    Refactor: moved free function

--- 3_Philosophers/philo_bonus/main_bonus.c
+++ 3_Philosophers/philo_bonus/main_bonus.c
@@ -6,11 +6,13 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:46:31 by daeha             #+#    #+#             */
-/*   Updated: 2024/07/09 03:46:23 by daeha            ###   ########.fr       */
+/*   Updated: 2024/07/16 15:47:16 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
+
+static void	free_resources(t_stat *stat);
 
 int	main(int argc, char **argv)
 {
@@ -21,4 +23,17 @@ int	main(int argc, char **argv)
 	control_procs(&stat);
 	free_resources(&stat);
 	return (0);
+}
+
+static void	free_resources(t_stat *stat)
+{
+	sem_close(stat->start);
+	sem_close(stat->forks);
+	sem_close(stat->write);
+	sem_unlink("start");
+	sem_unlink("forks");
+	sem_unlink("write");
+	free(stat->pids);
+	memset(&stat->philo, 0, sizeof(t_philo));
+	memset(stat, 0, sizeof(t_stat));
 }
