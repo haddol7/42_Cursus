e08d50b03b217d647f9f66d7dcbcfc97ad588ab6
haddol7 <haddol010313@khu.ac.kr>
2024-07-16 16:41:21 +0900

    Add: added start semaphore mutex & deleted eat semaphore

--- 3_Philosophers/philo_bonus/init_bonus.c
+++ 3_Philosophers/philo_bonus/init_bonus.c
@@ -6,7 +6,7 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 19:40:48 by daeha             #+#    #+#             */
-/*   Updated: 2024/07/11 21:12:13 by daeha            ###   ########.fr       */
+/*   Updated: 2024/07/16 16:34:27 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,35 +52,32 @@ static void	init_semaphore(t_stat *stat)
 {
 	sem_unlink("forks");
 	sem_unlink("write");
-	sem_unlink("eat");
+	sem_unlink("start");
 	stat->forks = sem_open("forks", O_CREAT, 0644, stat->num_philos);
 	stat->write = sem_open("write", O_CREAT, 0644, 1);
-	stat->eat = sem_open("eat", O_CREAT, 0644, 1);
+	stat->start = sem_open("start", O_CREAT, 0644, 1);
 	stat->pids = malloc(sizeof(pid_t) * stat->num_philos);
 	if (stat->pids == NULL)
 	{	
 		write(2, "Malloc error\n", 14);
 		sem_close(stat->forks);
 		sem_close(stat->write);
-		sem_close(stat->eat);
+		sem_close(stat->start);
 		sem_unlink("forks");
 		sem_unlink("write");
-		sem_unlink("eat");
+		sem_unlink("start");
 		exit(EXIT_FAILURE);
 	}
 }
 
 static void	init_philo(t_stat *stat)
-{	
-	stat->philo.name = 0;
-	stat->philo.birth_time = ft_gettime();
-	stat->philo.last_meal = stat->philo.birth_time;
+{
 	stat->philo.time_to_die = stat->time_to_die;
 	stat->philo.time_to_eat = stat->time_to_eat;
 	stat->philo.time_to_sleep = stat->time_to_sleep;
 	stat->philo.count_meal = stat->count_meal;
 	stat->philo.current_meal = 0;
+	stat->philo.start = stat->start;
 	stat->philo.forks = stat->forks;
 	stat->philo.write = stat->write;
-	stat->philo.eat = stat->eat;
 }
