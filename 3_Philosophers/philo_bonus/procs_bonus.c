71a8b8551a86723edafa0fb17f16e5ecd9fe459d
haddol7 <haddol010313@khu.ac.kr>
2024-07-16 16:44:39 +0900

    Add: added start flag feature and split functions

--- 3_Philosophers/philo_bonus/procs_bonus.c
+++ 3_Philosophers/philo_bonus/procs_bonus.c
@@ -6,26 +6,48 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 16:21:15 by daeha             #+#    #+#             */
-/*   Updated: 2024/07/11 20:24:50 by daeha            ###   ########.fr       */
+/*   Updated: 2024/07/16 16:33:38 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
+static void	wait_procs(t_stat *stat);
+
 void	control_procs(t_stat *stat)
 {
-	int	status;
-	int	i;
+	t_philo	*philo;
+	int		i;
 
-	status = EXIT_SUCCESS;
 	i = -1;
+	philo = &stat->philo;
+	sem_wait(stat->start);
 	while (++i < stat->num_philos)
 	{
-		stat->philo.name = i + 1;
+		philo->name = i + 1;
+		philo->str_name = ft_itoa(philo->name);
+		sem_unlink(philo->str_name);
+		philo->unique_eat = sem_open(philo->str_name, O_CREAT, 0644, 1);
+		if (philo->unique_eat == SEM_FAILED)
+			exit(EXIT_FAILURE);
+		philo->birth_time = ft_gettime();
+		philo->last_meal = philo->birth_time;
 		stat->pids[i] = fork();
 		if (!stat->pids[i])
 			proc_philos(stat);
+		sem_close(philo->unique_eat);
+		free(philo->str_name);
 	}
+	sem_post(stat->start);
+	wait_procs(stat);
+}
+
+static void	wait_procs(t_stat *stat)
+{
+	int		status;
+	int		i;
+
+	status = EXIT_SUCCESS;
 	i = -1;
 	while (++i < stat->num_philos)
 	{
