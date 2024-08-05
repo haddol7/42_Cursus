8d86a33477530f96e966446aa965bf712521eb18
haddol7 <haddol010313@khu.ac.kr>
2024-08-05 20:52:10 +0900

    Add: changed ppm format to mlx img format

--- 4_minirt/src/print/print.c
+++ 4_minirt/src/print/print.c
@@ -1,9 +1,12 @@
 #include "print.h"
 #include "utils.h"
 
-void write_color(t_color3 pixel_color)
+void write_color(t_img *img, t_color3 pixel_color, int x, int y)
 {
-	printf("%d %d %d\n", (int)(255.999 * pixel_color.x / 100),\
-						 (int)(255.999 * pixel_color.y / 100),\
-					 	 (int)(255.999 * pixel_color.z / 100));
+	void	*img_addr;
+	
+	img_addr = img->addr + img->bits_per_pixel / 8 * x + img->line_size * y;
+	*(unsigned int *)img_addr = ((int)(255.999 * pixel_color.x / ANTI_SAMPLE) << 16) | \
+								((int)(255.999 * pixel_color.y / ANTI_SAMPLE) << 8) | \
+								(int)(255.999 * pixel_color.z / ANTI_SAMPLE);
 }
