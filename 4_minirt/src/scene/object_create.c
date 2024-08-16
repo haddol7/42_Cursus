34ca7f6868d131d088d72962dba657fa2db49c03
haddol7 <haddol010313@khu.ac.kr>
2024-08-16 15:51:11 +0900

    Add: Texture info

--- 4_minirt/src/scene/object_create.c
+++ 4_minirt/src/scene/object_create.c
@@ -1,15 +1,43 @@
-#include "structures.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   object_create.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 15:36:27 by daeha             #+#    #+#             */
+/*   Updated: 2024/08/16 15:36:28 by daeha            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-t_object *object(t_object_type type, void *element, t_color3 albedo)
+#include "structures.h"
+#include "libft.h"
+
+t_object *object(t_object_type type, void *element, t_color3 albedo, char *texture, void *mlx)
 {
 	t_object *new;
 
 	if (!(new = (t_object *)malloc(sizeof(t_object))))
 		return (NULL);
+	ft_memset(new, 0, sizeof(t_object));
 	new->type = type;
 	new->element = element;
 	new->next = NULL;
 	new->albedo = albedo;
+	if (texture && ft_strcmp(texture, "CHECKER") == 0)
+		new->is_checker = TRUE;
+	else if (texture)
+	{
+		new->texture.img.id = mlx_xpm_file_to_image(mlx, texture, &new->texture.width, &new->texture.height);
+		if (new->texture.img.id == NULL)
+		{
+			write(STDERR_FILENO, "image file error\n", 18);
+			new->is_checker = TRUE;
+		} 
+		else
+			new->texture.img.addr = mlx_get_data_addr(new->texture.img.id, &new->texture.img.bits_per_pixel, \
+														&new->texture.img.line_size, &new->texture.img.endian);
+	}
 	return (new);
 }
 
