bc78366e33c24f2810b865b86677e96a2382dd73
haddol7 <haddol010313@khu.ac.kr>
2024-08-19 19:11:40 +0900

    Norm

--- 4_minirt/src/utils/objects_utils.c
+++ 4_minirt/src/utils/objects_utils.c
@@ -6,7 +6,7 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 15:37:06 by daeha             #+#    #+#             */
-/*   Updated: 2024/08/16 15:37:07 by daeha            ###   ########.fr       */
+/*   Updated: 2024/08/19 19:11:19 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	oadd(t_object **list, t_object *new)
 {
-	t_object *cur;
+	t_object	*cur;
 
 	if (*list == NULL)
 	{
@@ -34,4 +34,15 @@ t_object	*olast(t_object *list)
 	while (list->next)
 		list = list->next;
 	return (list);
+}
+
+int	terminate(void *data_addr)
+{
+	t_data	*data;
+
+	data = (t_data *)data_addr;
+	mlx_destroy_image(data->engine->mlx, data->engine->img->id);
+	mlx_destroy_window(data->engine->mlx, data->engine->win);
+	printf("\033[0;92mProgram closed!\n");
+	exit(0);
 }
--- 4_minirt/src/utils/vector/vector_4.c
+++ 4_minirt/src/utils/vector/vector_4.c
@@ -1,23 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   vector_4.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/16 15:37:12 by daeha             #+#    #+#             */
-/*   Updated: 2024/08/19 19:08:15 by daeha            ###   ########.fr       */
+/*   Created: 2024/08/19 19:07:37 by daeha             #+#    #+#             */
+/*   Updated: 2024/08/19 19:08:35 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
-double	drandom(void)
+t_vec3	vunit(t_vec3 vec)
 {
-	return ((float)rand() / (RAND_MAX));
+	double	len;
+
+	len = vlength(vec);
+	if (len == 0)
+	{
+		printf("Divide ERR\n");
+		exit(1);
+	}
+	vec.x /= len;
+	vec.y /= len;
+	vec.z /= len;
+	return (vec);
 }
 
-double	drandom_(double min, double max)
+t_vec3	vmin(t_vec3 vec1, t_vec3 vec2)
 {
-	return (min + (max - min) * drandom());
+	if (vec1.x > vec2.x)
+		vec1.x = vec2.x;
+	if (vec1.y > vec2.y)
+		vec1.y = vec2.y;
+	if (vec1.z > vec2.z)
+		vec1.z = vec2.z;
+	return (vec1);
 }
