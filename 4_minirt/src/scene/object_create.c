548be120566d65852f74bf389aaa9b22c1fe6a63
haddol7 <haddol010313@khu.ac.kr>
2024-08-20 15:17:21 +0900

    Norm: object functions I HATE NORM

--- 4_minirt/src/scene/object_create.c
+++ 4_minirt/src/scene/object_create.c
@@ -6,18 +6,16 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 15:36:27 by daeha             #+#    #+#             */
-/*   Updated: 2024/08/19 18:21:29 by daeha            ###   ########.fr       */
+/*   Updated: 2024/08/20 15:15:49 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "structures.h"
 #include "libft.h"
 
-// t_object *object(t_)
-
-t_object *object_default(t_object_type type, void *element, t_color3 albedo)
+t_object	*object_default(t_object_type type, void *element, t_color3 albedo)
 {
-	t_object *new;
+	t_object	*new;
 
 	new = (t_object *)ft_malloc(sizeof(t_object));
 	new->type = type;
@@ -27,9 +25,10 @@ t_object *object_default(t_object_type type, void *element, t_color3 albedo)
 	return (new);
 }
 
-t_object	*object_texture(t_object_type type, void *element, char *texture, void *mlx)
+t_object	*object_texture(t_object_type type, \
+							void *element, char *texture, void *mlx)
 {
-	t_object *new;
+	t_object	*new;
 
 	new = (t_object *)ft_malloc(sizeof(t_object));
 	new->type = type;
@@ -41,31 +40,23 @@ t_object	*object_texture(t_object_type type, void *element, char *texture, void 
 	{
 		new->bump.img.id = mlx_xpm_file_to_image(mlx, "./brick_normal.xpm", &new->bump.width, &new->bump.height);
 		new->bump.img.addr = mlx_get_data_addr(new->bump.img.id, &new->bump.img.bits_per_pixel, &new->bump.img.line_size, &new->bump.img.endian);
-		
 		new->texture.img.id = mlx_xpm_file_to_image(mlx, texture, &new->texture.width, &new->texture.height);
 		if (new->texture.img.id == NULL)
 		{
 			write(STDERR_FILENO, "image file error\n", 18);
-			//new->is_checker = TRUE;
-		} 
+			new->is_checker = TRUE;
+		}
 		else
-			new->texture.img.addr = mlx_get_data_addr(new->texture.img.id, &new->texture.img.bits_per_pixel, \
-														&new->texture.img.line_size, &new->texture.img.endian);
+			new->texture.img.addr = mlx_get_data_addr(new->texture.img.id, \
+			&new->texture.img.bits_per_pixel, \
+			&new->texture.img.line_size, &new->texture.img.endian);
 	}
 	return (new);
 }
 
-// void	*categorize(t_object_type type)
-// {
-// 	void	*ob;
-
-// 	if (type == SP)
-// 		ob = sphere()
-// }
-
-t_sphere *sphere(t_point3 center, double radius)
+t_sphere	*sphere(t_point3 center, double radius)
 {
-	t_sphere *sp;
+	t_sphere	*sp;
 
 	sp = (t_sphere *)ft_malloc(sizeof(t_sphere));
 	sp->center = center;
@@ -74,7 +65,8 @@ t_sphere *sphere(t_point3 center, double radius)
 	return (sp);
 }
 
-t_cylinder	*cylinder(t_point3 center, t_vec3 normal, double radius, double height)
+t_cylinder	*cylinder(t_point3 center, t_vec3 normal, \
+						double radius, double height)
 {
 	t_cylinder	*cy;
 
@@ -87,7 +79,7 @@ t_cylinder	*cylinder(t_point3 center, t_vec3 normal, double radius, double heigh
 	return (cy);
 }
 
-t_plane		*plane(t_point3 center, t_vec3 normal)
+t_plane	*plane(t_point3 center, t_vec3 normal)
 {
 	t_plane	*pl;
 
@@ -97,13 +89,14 @@ t_plane		*plane(t_point3 center, t_vec3 normal)
 	return (pl);
 }
 
-t_light     *light_point(t_point3 light_origin, t_color3 light_color, double bright_ratio)
+t_light	*light_point(t_point3 light_origin, \
+					t_color3 light_color, double bright_ratio)
 {
-    t_light *light;
+	t_light	*light;
 
-    light = (t_light *)ft_malloc(sizeof(t_light));
-    light->origin = light_origin;
-    light->light_color = light_color;
-    light->bright_ratio = bright_ratio;
-    return (light);
+	light = (t_light *)ft_malloc(sizeof(t_light));
+	light->origin = light_origin;
+	light->light_color = light_color;
+	light->bright_ratio = bright_ratio;
+	return (light);
 }
