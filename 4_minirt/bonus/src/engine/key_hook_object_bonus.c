abdb08f84df39bffdbc59dd7bd8a323ba16fee92
haddol7 <haddol010313@khu.ac.kr>
2024-09-09 21:38:42 +0900

    ADD: Light selection

--- 4_minirt/bonus/src/engine/key_hook_object_bonus.c
+++ 4_minirt/bonus/src/engine/key_hook_object_bonus.c
@@ -6,11 +6,13 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 23:36:19 by daeha             #+#    #+#             */
-/*   Updated: 2024/09/07 18:51:04 by daeha            ###   ########.fr       */
+/*   Updated: 2024/09/09 21:36:29 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt_bonus.h"
+
+static t_object	*next_light_obj(t_data *data);
 
 void	object_translation(int keycode, t_vec3 *center, t_data *data)
 {
@@ -30,7 +32,7 @@ void	object_translation(int keycode, t_vec3 *center, t_data *data)
 	else if (keycode == KEY_E)
 		*center = vplus_(*center, 0, -1, 0);
 	else if (keycode == KEY_L)
-		data->scene->selected_obj = data->scene->light;
+		data->scene->selected_obj = next_light_obj(data);
 	else if (keycode == KEY_ESC)
 		data->scene->selected_obj = NULL;
 }
@@ -49,4 +51,15 @@ void	object_rotation(int keycode, t_vec3 *normal)
 		rotation(normal, vec3(0, 0, 1), 5);
 	else if (keycode == KEY_SIX)
 		rotation(normal, vec3(0, 0, 1), -5);
+}
+
+static t_object	*next_light_obj(t_data *data)
+{
+	t_object	*obj;
+	
+	obj = data->scene->selected_obj;
+	if (obj && obj->type == LIGHT)
+		return (obj->next);
+	else
+		return (data->scene->light);
 }
