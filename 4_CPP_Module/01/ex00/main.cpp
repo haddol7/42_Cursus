6e41d5f0d6d620580c7117b1dcacf2fb011d7658
haddol7 <haddol010313@khu.ac.kr>
2024-10-06 00:25:44 +0900

    Fix: stack memory

--- 4_CPP_Module/01/ex00/main.cpp
+++ 4_CPP_Module/01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 20:37:22 by daeha             #+#    #+#             */
-/*   Updated: 2024/09/29 21:09:59 by daeha            ###   ########.fr       */
+/*   Updated: 2024/10/06 00:24:43 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 int main(void)
 {
 	Zombie *daeha = newZombie("daeha");
+	Zombie test("test");
 
+	test.announce();
 	daeha->announce();
+	delete daeha;
 	randomChump("one");
 	randomChump("two");
 	randomChump("three");
 
-	delete daeha;
 	return (0);
 }
