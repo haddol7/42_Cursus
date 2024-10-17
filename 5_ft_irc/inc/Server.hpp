2975e56ac063b71a5399d5c71409a8aa73deed64
haddol7 <haddol010313@khu.ac.kr>
2024-10-17 09:29:30 +0000

    feat: Add test code

--- 5_ft_irc/inc/Server.hpp
+++ 5_ft_irc/inc/Server.hpp
@@ -3,12 +3,12 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <sys/epoll.h>
+#include <vector>
 
 //TODO : 매크로 따로 옮기고 사이즈 설정 해야 함! 
 # define LISTEN_SIZE (5)
 # define EPOLL_SIZE (5)
 # define BUF_SIZE (600)
-
 
 //싱글톤 패턴
 class Server
@@ -16,18 +16,21 @@ class Server
 public:
 	static Server* GetServer();
 	void InitServer(const char* port);
-	void ExecLoopServer(void);
-	void CloseServerFd();
+	void ExecServerLoop(void);
 
 private:
 	static Server* mInstance;
-	int	mEpfd;
 	struct epoll_event *mEpollEvents;
-	
+	int	mEpfd;
 	int mSocket;
+	
 	//server name
 	//password
 	//map<nick <client>>
 	//map<tag, <channel>>
 	Server();
+	void registerClient();
+	void readBufferFromClientLoop(const int i_event);
+
+	std::vector<int> test_ClientFd;
 };
