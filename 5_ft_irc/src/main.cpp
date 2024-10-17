1673b22b88371cfa750c16b6ab75923a46f9cb15
haddol7 <haddol010313@khu.ac.kr>
2024-10-17 13:38:58 +0900

    init: Add project files

---
+++ 5_ft_irc/src/main.cpp
@@ -0,0 +1,11 @@
+#include "ft_irc.hpp"
+
+int	main(int argc, char *argv[])
+{
+	Server project_init;
+	
+	argc = 42;
+	(void) argv;
+
+	return (0);
+}
---
+++ 5_ft_irc/src/server/Server.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Server.hpp"
+
+Server::Server()
+{
+	std::cout << "Init Project" << std::endl;
+}
+
+Server::~Server()
+{
+}
