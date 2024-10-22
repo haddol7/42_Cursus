abaf86cb868e82cf0c749fec4c1b2f3bb21b7b78
haddol7 <haddol010313@khu.ac.kr>
2024-10-22 13:24:22 +0000

    fix: 닉네임이 제대로 파싱되도록 수정

--- 5_ft_irc/src/message/Nick.cpp
+++ 5_ft_irc/src/message/Nick.cpp
@@ -12,12 +12,16 @@ AMessage(origin, "Nick", msg)
 	pos = mBuff.find("NICK");
 	pos = mBuff.find(" ", pos);
 
-	if (pos != std::string::npos && ++pos != mBuff.length())
-		mNick.append(mBuff, pos, mBuff.length() - pos);
+	// if (pos != std::string::npos && ++pos != mBuff.length())
+	// 	mNick.append(mBuff, pos, mBuff.length() - 1 - pos);
+	// pos = mBuff.find(" ");
+	mNick = mBuff.substr(pos + 1, mBuff.length() - 7);
+	//TODO : 예외 상황 체크
 }
 
 void	Nick::ExecuteCommand()
-{
+{	
+	std::cerr << "adasd >> " << mNick << std::endl;
 	if (mNick.length() == 0)
 		ReplyToOrigin(ERR_NONICKNAMEGIVEN);
 	else if (isNickInvalid())
@@ -36,7 +40,7 @@ static bool	checkLetter(char c)
 {
 	if (std::isalnum(c))
 		return 0;
-	else if ((c >= 0x5B && c <= 60) || (c >= 0x7B && c <= 0x7D) || c == '-')
+	else if ((c >= 0x5B && c <= 0x60) || (c >= 0x7B && c <= 0x7D) || c == '-')
 		return 0;
 	return 1;
 }
@@ -58,9 +62,9 @@ bool	Nick::isNickInvalid() const
 bool	Nick::isNickDuplicated() const
 {
 	Server	*server = Server::GetServer();
+	Client	*client = server->ReturnClientOrNull(mNick);
 
-	server->ReturnClientOrNull(mNick);
-	if (server)
-		return 1;
-	return 0;
+	if (client == NULL)
+		return false;
+	return true;
 }
