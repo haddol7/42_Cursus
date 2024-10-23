cc65778e4a37a3cf7435071b144b51eb451fc07a
jsuk <tjrwngh2@gmail.com>
2024-10-23 05:15:44 +0000

    refactor: AMessage.cpp Nick.cpp file string 관련 부분 refactoring

--- 5_ft_irc/src/message/Nick.cpp
+++ 5_ft_irc/src/message/Nick.cpp
@@ -8,15 +8,15 @@ Nick::Nick(Client* origin, const std::string msg) :
 AMessage(origin, "Nick", msg)
 {
 	std::string::size_type	pos;
+	std::string::size_type	end;
 
 	pos = mBuff.find("NICK");
 	pos = mBuff.find(" ", pos);
 
-	// if (pos != std::string::npos && ++pos != mBuff.length())
-	// 	mNick.append(mBuff, pos, mBuff.length() - 1 - pos);
-	// pos = mBuff.find(" ");
-	mNick = mBuff.substr(pos + 1, mBuff.length() - 7);
-	//TODO : 예외 상황 체크
+	end = mBuff.find("\r\n");
+
+	if (pos != std::string::npos && ++pos != end)
+		mNick = mBuff.substr(pos, end - pos);
 }
 
 void	Nick::ExecuteCommand()
@@ -35,35 +35,34 @@ Nick::~Nick()
 {
 }
 
-static bool	checkLetter(char c)
+static bool	isCharacterNotAllowed(char c)
 {
 	if (std::isalnum(c))
-		return 0;
+		return false;
 	else if ((c >= 0x5B && c <= 0x60) || (c >= 0x7B && c <= 0x7D) || c == '-')
-		return 0;
-	return 1;
+		return false;
+	return true;
 }
 
 bool	Nick::isNickInvalid() const
 {
 	if (mNick.length() > 9)
-		return 1;
+		return true;
 	else if (std::isdigit(mNick[0]) || mNick[0] == '-')
-		return 1;
+		return true;
 	for (unsigned int i = 0; i < mNick.length(); ++i)
 	{
-		if (checkLetter(mNick[i]))
-			return 1;
+		if (isCharacterNotAllowed(mNick[i]))
+			return true;
 	}
-	return 0;
+	return false;
 }
 
 bool	Nick::isNickDuplicated() const
 {
 	Server	*server = Server::GetServer();
-	Client	*client = server->ReturnClientOrNull(mNick);
 
-	if (client == NULL)
-		return false;
-	return true;
+	if (server->ReturnClientOrNull(mNick))
+		return true;
+	return false;
 }
