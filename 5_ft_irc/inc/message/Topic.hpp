6cdd96116d612c895d6f89543be43a625aaa1488
haddol7 <haddol010313@khu.ac.kr>
2024-10-25 04:58:42 +0000

    feat: Implement TOPIC command

--- 5_ft_irc/inc/message/Topic.hpp
+++ 5_ft_irc/inc/message/Topic.hpp
@@ -4,11 +4,21 @@
 
 class Topic : public AMessage
 {
-	public:
-		Topic(Client* origin, const std::string msg);
-		virtual void	ExecuteCommand();
-		virtual	~Topic();
+public:
+	Topic(Client* origin, const std::string msg);
+	virtual void	ExecuteCommand();
+	bool			Isvalidchannel();
+	bool			IsOperatorOrReplyInfo();
+	void			SetTopic();
+	void			SendReply() const;
+	bool			FindChannel();
+	virtual			~Topic();
 
-	private:
-		Topic();
+private:
+	Topic();
+	std::string	mTargetChannel;
+	std::string	mTargetTopic;
+	std::string	mOriginNick;
+	Channel		*mChannel;
+
 };
