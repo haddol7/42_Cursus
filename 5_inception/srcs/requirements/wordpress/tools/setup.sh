bf88ab67151f309c1574889ac668c4171e4b26a4
haddol7 <haddol010313@khu.ac.kr>
2024-11-25 17:53:57 +0900

    asdasdasdjklads

--- 5_inception/srcs/requirements/wordpress/tools/setup.sh
+++ 5_inception/srcs/requirements/wordpress/tools/setup.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-# this script run in the building container
-# it changes the ownership of the /var/www/inception/ folder to www-data user
-# then sure that the wp-config.php file is in the /var/www/inception/ folder
-# then it downloads the wordpress core files if they are not already there
-# then it installs wordpress if it is not already installed
-# and set the admin user and password if they are not already set
-# this variables are set in the .env file
-# the penultimate line download and activate the raft theme, that I liked most
-# at the end, exec $@ run the next CMD in the Dockerfile.
-# In this case: starts the php-fpm7.4 server in the foreground
-
-# set -ex # print commands & exit on error (debug mode)
-
-# WP_URL=login.42.fr
-# WP_TITLE=Inception
-# WP_ADMIN_USER=theroot
-# WP_ADMIN_PASSWORD=123
-# WP_ADMIN_EMAIL=theroot@123.com
-# WP_USER=theuser
-# WP_PASSWORD=abc
-# WP_EMAIL=theuser@123.com
-# WP_ROLE=editor
-
 chown -R www-data:www-data /var/www/inception/
 
 if [ ! -f "/var/www/inception/wp-config.php" ]; then
@@ -51,7 +28,5 @@ then
         --user_pass=$WP_PASSWORD \
         --role=$WP_ROLE
 fi;
-
-wp --allow-root --path="/var/www/inception/" theme install raft --activate 
 
 exec $@
