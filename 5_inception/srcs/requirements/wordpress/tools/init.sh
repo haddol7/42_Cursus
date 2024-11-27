ea73e140bcf7ee8f22048c513e1e408c664264e2
haddol7 <haddol010313@khu.ac.kr>
2024-11-27 17:11:37 +0900

    IHATETHIS!

--- 5_inception/srcs/requirements/wordpress/tools/init.sh
+++ 5_inception/srcs/requirements/wordpress/tools/init.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-if grep -q ";cgi.fix_pathinfo=1" "/etc/php/7.4/fpm/php.ini"; then
-    sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' "/etc/php/7.4/fpm/php.ini"
-fi
-
-if grep -q "listen = /run/php/php7.4-fpm.sock" "/etc/php/7.4/fpm/pool.d/www.conf"; then
-    sed -i "s|listen = /run/php/php7.4-fpm.sock|listen = 9000|g" /etc/php/7.4/fpm/pool.d/www.conf
-    sed -i 's/;listen.mode = 0660/listen.mode = 0660/g' /etc/php/7.4/fpm/pool.d/www.conf
-    sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php/7.4/fpm/pool.d/www.conf
-fi
-
 WP_PATH="/var/www/inception"
 WP_CONFIG="$WP_PATH/wp-config.php"
-
 chown www-data:www-data "$WP_CONFIG"
 
 if [ ! -f "$WP_CONFIG" ]; then
-    echo "Moving wp-config.php to $WP_PATH..."
+    echo "<Inception Debug> Init config in $WP_PATH..."
+    sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' "/etc/php/7.4/fpm/php.ini"
+    sed -i "s|listen = /run/php/php7.4-fpm.sock|listen = 9000|g" /etc/php/7.4/fpm/pool.d/www.conf
+    sed -i 's/;listen.mode = 0660/listen.mode = 0660/g' /etc/php/7.4/fpm/pool.d/www.conf
+    sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php/7.4/fpm/pool.d/www.conf
     wget --no-check-certificate https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
@@ -27,9 +20,8 @@ if [ ! -f "$WP_CONFIG" ]; then
 fi
 
 if [ ! -f "$WP_PATH/wp-load.php" ]; then
-    echo "Downloading WordPress..."
+    echo "<Inception Debug> Downloading WordPress..."
     wp --allow-root --path="$WP_PATH" core download
-    echo "Installing WordPress..."
     wp --allow-root --path="$WP_PATH" core install \
         --url=$WP_URL \
         --title=$WP_TITLE \
