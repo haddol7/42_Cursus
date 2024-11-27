be1f8022f02d3f439358da2274b4132ebcb5d59b
haddol7 <haddol010313@khu.ac.kr>
2024-11-27 21:52:32 +0900

    IHATETHIS!

--- 5_inception/srcs/requirements/wordpress/tools/init.sh
+++ 5_inception/srcs/requirements/wordpress/tools/init.sh
@@ -12,8 +12,6 @@ if [ ! -f "$WP_CONFIG" ]; then
     wget --no-check-certificate https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
-    mkdir -p /run/php/
-    mkdir -p /var/run/php/
     mkdir -p /var/www/inception/
     mv /tmp/wp-config.php "$WP_PATH"
 fi
@@ -22,20 +20,22 @@ chown www-data:www-data "$WP_CONFIG"
 
 if [ ! -f "$WP_PATH/wp-load.php" ]; then
     echo "<Inception Debug> Downloading WordPress..."
-    wp --allow-root --path="$WP_PATH" core download
+    wp core download --allow-root --path="$WP_PATH"
 	
-    wp --allow-root --path="$WP_PATH" core install \
-        --url=$WP_URL \
+    wp core install \
+    	--url=$WP_URL \
         --title=$WP_TITLE \
         --admin_user=$WP_ADMIN_USER \
         --admin_password=$WP_ADMIN_PASSWORD \
-        --admin_email=$WP_ADMIN_EMAIL
+        --admin_email=$WP_ADMIN_EMAIL \
+		--allow-root --path="$WP_PATH"
 
-    wp --allow-root --path="$WP_PATH" user create \
+    wp user create \
         $WP_USER \
         $WP_EMAIL \
         --user_pass=$WP_PASSWORD \
-        --role=$WP_ROLE
+        --role=$WP_ROLE \
+		--allow-root --path="$WP_PATH" 
 fi
 
 exec php-fpm7.4 --nodaemonize
