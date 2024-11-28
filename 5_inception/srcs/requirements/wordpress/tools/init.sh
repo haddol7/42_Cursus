473bd5e46a7ab5202b384cafd23893491cda7324
haddol7 <haddol010313@khu.ac.kr>
2024-11-28 15:22:50 +0900

    IHATETHIS!

--- 5_inception/srcs/requirements/wordpress/tools/init.sh
+++ 5_inception/srcs/requirements/wordpress/tools/init.sh
@@ -6,9 +6,6 @@ WP_CONFIG="$WP_PATH/wp-config.php"
 if [ ! -f "$WP_CONFIG" ]; then
     echo "<Inception Debug> Init config in $WP_PATH..."
     sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' "/etc/php/7.4/fpm/php.ini"
-    sed -i "s|listen = /run/php/php7.4-fpm.sock|listen = 9000|g" /etc/php/7.4/fpm/pool.d/www.conf
-    sed -i 's/;listen.mode = 0660/listen.mode = 0660/g' /etc/php/7.4/fpm/pool.d/www.conf
-    sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php/7.4/fpm/pool.d/www.conf
     wget --no-check-certificate https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
 	mkdir -p /run/php
