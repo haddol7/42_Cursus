68aa68b90f643f5e014821c151616e82adce092f
kapustin <kapustin4613@gmail.com>
2025-02-05 16:38:23 +0900

    refactor

--- 6_ft_transcendence/backend/user/server/config/urls.py
+++ 6_ft_transcendence/backend/user/server/config/urls.py
@@ -25,4 +25,3 @@ urlpatterns = [
     path('_internal/dashboard', views.InternalDashBoardView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- 6_ft_transcendence/backend/user/server/user/apps.py
+++ 6_ft_transcendence/backend/user/server/user/apps.py
@@ -5,5 +5,3 @@ class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user'
 
-    # def ready(self):
-    #     import user.signals
