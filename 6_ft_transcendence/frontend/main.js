2ce572a9f13f4fd541385ccb750d0b9167b1c4f7
budnarae <budnarae1001@gmail.com>
2025-02-14 15:32:31 +0900

    feat: 다음의 사항들을 구현
    1. ctrl +로 페이지 요소가 화면 밖을 빠져나갈 때 스크롤이 적용되도록 함
    2. 뒤로 가기를 통해 friend 페이지로 접근했을 때 가장 최근에 조회했던
       friend의 정보가 뜨도록 함

--- 6_ft_transcendence/frontend/main.js
+++ 6_ft_transcendence/frontend/main.js
@@ -4,7 +4,20 @@ import { TwoFAPage } from "./modules/page/twoFA.mjs";
 import { FTOauth } from "./modules/authentication/ftOauth.mjs";
 import { JWT } from "./modules/authentication/jwt.mjs";
 
+const whenResize = () => {
+  if (
+    window.innerWidth < document.body.scrollWidth ||
+    window.innerHeight < document.body.scrollHeight
+  ) {
+    document.body.style.overflow = "auto";
+  } else {
+    document.body.style.overflow = "hidden";
+  }
+};
+
 document.addEventListener("DOMContentLoaded", async () => {
+  window.addEventListener("resize", whenResize);
+
   window.addEventListener("popstate", PageManager.popStateEvent);
   window.addEventListener("unload", function () {
     JWT.clearCookie();
