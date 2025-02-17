903586984e9794b67304163ec49a7a257372aa57
bagger3025 <bagger3025@g.skku.edu>
2025-02-17 17:05:54 +0900

    fix: database not found

--- 6_ft_transcendence/backend/game/gameapp/apps.py
+++ 6_ft_transcendence/backend/game/gameapp/apps.py
@@ -1,16 +1,6 @@
 from django.apps import AppConfig
-from requests import delete
 
 
 class GameappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "gameapp"
-
-    def ready(self):
-        # importing model classes
-        from .models import TempMatchUser, TempMatch, TempMatchRoomUser, TempMatchRoom
-
-        TempMatchUser.objects.all().delete()
-        TempMatch.objects.all().delete()
-        TempMatchRoomUser.objects.all().delete()
-        TempMatchRoom.objects.all().delete()
